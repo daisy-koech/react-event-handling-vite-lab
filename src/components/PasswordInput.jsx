2e77c49e32234576d2430a1389c8e6a996184b88
Daisy Koech <koechdaisy86@gmail.com>
2026-05-03 09:48:42 +0300

    event handlers added

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,15 @@
-// Code PasswordInput Component Here
+import React from 'react';
 
 function PasswordInput (){
+    function handleChange() {
+        console.log("Entering password...")
+    }
     return(
-        <></>
+        <input type="password" placeholder="Enter your password" onChange={handleChange}/>
     )
 }
 
 export default PasswordInput;
+
+// PasswordInput:
+// Add a change event to the <input>
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,21 @@
-// Code SubmitButton Component Here
+import React from 'react';
 
 function SubmitButton (){
+    function handleEnter() {
+        console.log("Mouse Entering");
+    }
+    function handleLeave() {
+        console.log("Mouse Exiting")
+    }
     return(
-        <></>
+        <button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Submit Password</button>
     )
 }
 
 export default SubmitButton;
+
+// Create a SubmitButton React component
+// Render a <button> with the text “Submit Password”
+
+// Add a mouse enter event to the <button>
+// Add a mouse leave event to the <button>
